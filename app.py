75ed6e7417f52062689c12147894929934f37da0
Julian Rosse <julian@helixbass.net>
2013-10-16 16:42:40 -0400

    tests passing

--- app.py
+++ app.py
@@ -1,71 +1,91 @@
 from flask import Flask, url_for, render_template, request, redirect, abort, make_response
+from mongokit import Connection, Document, ValidationError as MongoValidationError
+from werkzeug.routing import BaseConverter, ValidationError
+from itsdangerous import base64_encode, base64_decode
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+MONGODB_DATABASE = 'epoch'
+
+class ObjectIDConverter(BaseConverter):
+    def to_python(self, value):
+        try:
+            return ObjectId(base64_decode(value))
+        except (InvalidId, ValueError, TypeError):
+            raise ValidationError()
+    def to_url(self, value):
+        return base64_encode(value.binary)
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+try:
+    app.config.from_envvar('EPOCH_CONFIG')
+except RuntimeError:
+    pass
+app.url_map.converters['objectid'] = ObjectIDConverter
+
+app.connection = connection = Connection(app.config['MONGODB_HOST'],
+                                         app.config['MONGODB_PORT'])
 
 @app.route('/')
 def index():
     return 'index'
 
-class Order(object):
+@connection.register
+class Order(Document):
 
-    stati = ('pending', 'sentToLab', 'shippedFromLab', 'receivedFromLab',
-             'shipped', 'notifiedOfShipment')
+    stati = (u'pending', u'sentToLab', u'shippedFromLab', u'receivedFromLab',
+             u'shipped', u'notifiedOfShipment')
 
-    _orders = []
+    __collection__ = 'orders'
+    __database__ = app.config['MONGODB_DATABASE']
 
-    @classmethod
-    def all(klass):
-        return klass._orders
+    structure = {
+        'name': basestring,
+        'status': basestring,
+    }
+    required_fields = ['name', 'status']
+    default_values = {
+        'status': u'pending'
+    }
+    validators = {
+        'status': lambda val: val in Order.stati
+    }
+    use_dot_notation = True
+    def __repr__(self):
+        return '<Order %r>' % self.name
 
-    @classmethod
-    def last(klass):
-        return klass._orders[-1]
-
-    @classmethod
-    def create(klass, name):
-        klass._orders.append(klass(name))
-
-    @classmethod
-    def find(klass, order_id):
-        for order in klass.all():
-            if order.id == order_id:
-                return order
-        abort(404)
-
-    def __init__(self, name):
-        try:
-            self.id = self.__class__.last().id + 1
-        except IndexError:
-            self.id = 1
-        self.name = name
-        self.status = self.__class__.stati[0]
-
-    @property
-    def status(self):
-        return self._status
-
-    @status.setter
-    def status(self, value):
-        if value not in self.__class__.stati:
-            abort(400)
-        self._status = value
+    def create_from_form(self, form):
+        order = self()
+        order.name = form['name']
+        order.save()
+        return order._id
 
 @app.route('/orders')
 def index_orders():
-    return render_template('orders.html', orders=Order.all())
+    return render_template('orders.html', orders=connection.Order.find())
 
 @app.route('/orders', methods=['POST'])
 def create_order():
-    Order.create(request.form['name'])
-    return make_response(render_template('order.html', order=Order.last()), 201)
+    _id = connection.Order.create_from_form(request.form)
+    return redirect(url_for('show_order', order_id=_id))
 
-@app.route('/orders/<int:order_id>')
+@app.route('/orders/<objectid:order_id>')
 def show_order(order_id):
-    return render_template('order.html', order=Order.find(order_id))
+    return render_template('order.html', order=connection.Order.find_one({'_id': order_id}))
 
-@app.route('/orders/<int:order_id>/status', methods=['PUT'])
+@app.route('/orders/<objectid:order_id>/status', methods=['PUT'])
 def update_order_status(order_id):
     status = request.form.get('orderStatus')
-    Order.find(order_id).status = status
+    # connection.Order.collection.update({'_id': order_id}, {'$set': {'status': status}})
+    doc = connection.Order.find_one({'_id': order_id})
+    doc.status = status
+    try:
+        doc.save()
+    except MongoValidationError:
+        abort(400)
     return '', 201
 
 if __name__ == '__main__':
--- test_app.py
+++ test_app.py
@@ -1,28 +1,43 @@
 import app as ap
-from pytest import fixture, raises
+from flask import url_for
+from pytest import fixture, raises, mark
+import re
+
+@fixture(scope='module')
+def app(request):
+    _app = ap.app
+    _app.testing = True
+    ctx = _app.test_request_context()
+    ctx.push()
+    def fin():
+        ctx.pop()
+    request.addfinalizer(fin)
+    return _app.test_client()
 
 @fixture
-def app():
-    return ap.app.test_client()
+def reset_db(app):
+    app.application.connection.drop_database(app.application.config['MONGODB_DATABASE'])
 
+@mark.usefixtures('reset_db')
 def test_orders_initially_empty(app):
     assert '<li>' not in app.get('/orders').data
 
 class TestOrder(object):
     @fixture(autouse=True)
-    def create_order(self, app):
-        app.post('/orders', data={ 'name': 'Test Order' })
+    def created_order_id(self, app):
+        resp = app.post('/orders', data={ 'name': 'Test Order' })
+        return re.search(r'''href=.+/([^/]+)['"]''', resp.data).group(1)
 
     def test_creates_order(self, app):
         assert '<li>' in app.get('/orders').data
 
-    def test_initial_pending_status(self, app):
-        assert 'pending' in app.get('/orders/1').data
+    def test_initial_pending_status(self, app, created_order_id):
+        assert 'pending' in app.get('/orders/%s' % created_order_id).data
 
-    def test_update_status(self, app):
-        app.put('/orders/1/status', data={ 'orderStatus': 'sentToLab' })
-        assert 'sentToLab' in app.get('/orders/1').data
+    def test_update_status(self, app, created_order_id):
+        app.put('/orders/%s/status' % created_order_id, data={ 'orderStatus': 'sentToLab' })
+        assert 'sentToLab' in app.get('/orders/%s' % created_order_id).data
 
-    def test_unknown_status_fails(self, app):
-        response = app.put('/orders/1/status', data={ 'orderStatus': 'INVALID' })
+    def test_unknown_status_fails(self, app, created_order_id):
+        response = app.put('/orders/%s/status' % created_order_id, data={ 'orderStatus': 'INVALID' })
         assert '400' in response.status
